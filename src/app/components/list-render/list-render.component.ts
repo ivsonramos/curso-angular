1eba75ef7583c8b4ccff5153d1d12766144c5f7f
ivson.ramos <ivson.ramos@gmail.com>
2023-02-10 20:29:57 -0300

    :sparkles:Adiciona interfaces ao angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.scss']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    {name: "Turca", Type: "Dog"},
-    {name: "Tom", Type: "Cat"},
-    {name:"Frida", Type: "Dog"},
-    {name:"Bod", Type: "Horse"},
-  ]
+  animals: Animal[] = [
+    {name: 'Turca', type: 'Dog', age: 4},
+    {name: 'Tom', type: 'Cat', age: 10},
+    {name: 'Frida', type: 'Dog', age: 5},
+    {name: 'Bod', type: 'Horse', age: 1},
+  ];
+
+  animal: Animal = {
+    name: 'teste',
+    type: 'alguma coisa',
+    age: 10,
+  }
+
+  animalDetails = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
   }
 
 }
