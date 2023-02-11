b3e61f39eef2b2345127194a45a6fbbe37373b38
ivson.ramos <ivson.ramos@gmail.com>
2023-02-10 21:03:31 -0300

    :sparkles:Adiciona pipe operators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ListRenderComponent } from './components/list-render/list-render.compon
     EventosComponent,
     EmitterComponent,
     ChangeNumberComponent,
-    ListRenderComponent
+    ListRenderComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
